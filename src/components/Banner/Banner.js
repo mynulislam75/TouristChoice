f428f20e3159811b788daefcfb2f7cceb6f49192
mynulislam75 <mynulislam181120@gmail.com>
2021-11-10 23:17:01 +0600

    seconds commit

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -4,16 +4,21 @@ import './Banner.css'
 const Banner = () => {
     return (
         <div>
-                <div className="banner-section">
-                <div className="row container">
-                    <div className="description-text position-absolute bottom-0 p-3 col-lg-6 col-md-6 col-sm-6">
-                        <h1>Specialties, Services & Treatments</h1>
-                        <p>We provide a full range of family care and more than 40 specialty and subspecialty health care services for patients in our service area.
-                            Explore specialized care or select a particular service to learn more.</p>
-                        <button className="bg-success btn-regular">JOIN US</button>
+            <div className="banners">
+                <div className=" banner row justify-content-center align-items-center">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-10">
+                                <div className="container">
+                                <h1 className="banner-title">Take care of your Health Now !</h1>
+                                <h3 className="sub-title">Sale up to 30% all products</h3>
+                                <button className="banner-btn">shop now</button>
+                                </div>
+                            </div>
+                            <div className="col-md-2"></div>
+                        </div>
                     </div>
                 </div>
-
             </div>
         </div>
     );
